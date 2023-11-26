667ff4ec8957420f75e56bd23f11e838d196b41e
Anton Pavlov <admin@MacBook-Pro.local>
2023-11-26 13:59:23 +0200

    Add prod css prefix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import React from "react";
+
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
